b10f18cd81a28bb6ed1cad34745ab88499fe6612
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-12-06 09:18:51 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,10 +7,10 @@ st.markdown("### 📑 COMPARADOR DE EMPENHOS")
 st.sidebar.title("Upload dos Arquivos")
 
     # Upload do primeiro CSV
-uploaded_file_1 = st.sidebar.file_uploader("Carregue abaixo o arquivo referente ao **relatório de 'empenhos emitios'** gerados pelos seu sistema:", type=["csv"])
+uploaded_file_1 = st.sidebar.file_uploader("Carregue abaixo o arquivo referente ao **relatório de 'empenhos emitios'** gerados pelos seu sistema 👇", type=["csv"])
 
     # Upload do segundo CSV
-uploaded_file_2 = st.sidebar.file_uploader("Carregue abaixo o arquivo referente aos **empenhos planilhados**:", type=["csv"])
+uploaded_file_2 = st.sidebar.file_uploader("Carregue abaixo o arquivo referente aos **empenhos planilhados** 👇", type=["csv"])
 
 if (uploaded_file_1 is not None) and (uploaded_file_2 is not None):
     df1 = pd.read_csv(uploaded_file_1)
