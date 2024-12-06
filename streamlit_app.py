8848820dce93281f52de6f85e26e9cb8ee8a84bf
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-12-06 10:07:14 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,10 +24,42 @@ if (uploaded_file_1 is not None) and (uploaded_file_2 is not None):
 
     # Criando um novo DataFrame com as colunas desejadas
     empenhos_planilhados = df2[['EMPENHO', 'ANO', 'PLANILHA']]
-    #empenhos_planilhados['PLANILHA']= empenhos_planilhados['PLANILHA'].str.decode('utf-8')
+    empenhos_relatorio = df1[['Empenho', 'Vínculo', 'Emissão,', 'Empenhado' 'Credor']]
 
-    st.data_editor(empenhos_planilhados)
+    # Função para comparar os DataFrames e retornar um novo DataFrame com as diferenças
+    def comparar_empenhos(df1, df2):
+        # Renomear a coluna 'Empenho' do df1 para garantir a comparação
+        df1 = df1.rename(columns={'Empenho': 'EMPENHO'})
 
+        # Encontrar os empenhos que estão apenas em um dos DataFrames
+        empenhos_unicos_df1 = df1[~df1['EMPENHO'].isin(df2['EMPENHO'])]
+        empenhos_unicos_df2 = df2[~df2['EMPENHO'].isin(df1['EMPENHO'])]
+
+        # Adicionar uma coluna para indicar a origem dos dados
+        empenhos_unicos_df1['ORIGEM'] = 'Relatório'
+        empenhos_unicos_df2['ORIGEM'] = 'Planilha'
+
+        # Concatenar os DataFrames de diferenças
+        diferencas = pd.concat([empenhos_unicos_df1, empenhos_unicos_df2], ignore_index=True)
+
+        return diferencas
+
+    # Chamar a função para comparar os DataFrames
+    diferencas = comparar_empenhos(empenhos_relatorio, empenhos_planilhados)
+
+    # Interface do Streamlit
+    #st.title("Comparação de Empenhos")
+
+    # Aba com as diferenças
+    with st.tab("Empenhos não encontrados em ambos"):
+        st.dataframe(diferencas)
+
+    # Aba com os dados originais (opcional)
+    with st.tab("Dados originais"):
+        st.subheader("Relatório")
+        st.dataframe(empenhos_relatorio)
+        st.subheader("Planilha")
+        st.dataframe(empenhos_planilhados)
 
 
 
