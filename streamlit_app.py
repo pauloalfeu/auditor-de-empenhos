9eb70cb9960b3cb48eb3f995ac5565fdb3bab7b9
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-12-06 09:16:35 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,15 +7,14 @@ st.markdown("### 📑 COMPARADOR DE EMPENHOS")
 st.sidebar.title("Upload dos Arquivos")
 
     # Upload do primeiro CSV
-uploaded_file_1 = st.sidebar.file_uploader("Carregue abaixo o arquivo referente ao relatório de 'empenhos emitios' gerados pelos seu sistema", type=["csv"])
-if uploaded_file_1 is not None:
+uploaded_file_1 = st.sidebar.file_uploader("Carregue abaixo o arquivo referente ao relatório de 'empenhos emitios' gerados pelos seu sistema:", type=["csv"])
+
+    # Upload do segundo CSV
+uploaded_file_2 = st.sidebar.file_uploader("Carregue abaixo o arquivo referente aos empenhos planilhados:", type=["csv"])
+
+if (uploaded_file_1 is not None) and (uploaded_file_2 is not None):
     df1 = pd.read_csv(uploaded_file_1)
     st.write("## Empenhos do Relatório")
     st.dataframe(df1)
-
-    # Upload do segundo CSV
-uploaded_file_2 = st.sidebar.file_uploader("Carregue abaixo o arquivo referente aos empenhos planilhados", type=["csv"])
-if uploaded_file_2 is not None:
-    df2 = pd.read_csv(uploaded_file_2)
-    st.write("## Empenhos Planilhados")
-    st.dataframe(df2)
+else:
+    st.info("☝️ Atenção: Para que a comparação seja realizada, é preciso que você faça o upload dos dois arquivos CSV indicados na barra lateral. Ao enviar, o sistema irá processar os dados e apresentar os resultados de forma clara e organizada.")
