03209ca56ea51b7e85204dde8ba9820be928ed53
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-12-06 10:40:15 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,8 @@ uploaded_file_1 = st.sidebar.file_uploader("Carregue abaixo o arquivo referente 
 uploaded_file_2 = st.sidebar.file_uploader("Carregue abaixo o arquivo referente aos **empenhos planilhados**:", type=["csv"])
 
 if (uploaded_file_1 is not None) and (uploaded_file_2 is not None):
-    df1 = pd.read_csv(uploaded_file_1, sep=';', encoding='latin1')
+    df = pd.read_csv(uploaded_file_1, sep=';', encoding='latin1')
+    df1 = pd.DataFrame(df)
     df2 = pd.read_csv(uploaded_file_2, sep=',', encoding='utf-8')
     st.data_editor(df1)
     # Criando uma nova coluna 'ano' extraindo o ano da coluna 'empenho'
