415f05bcbe19409b9d65258b29a045d4a6ce242c
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-12-06 09:11:27 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,18 +2,19 @@ import streamlit as st
 import pandas as pd
 
 st.set_page_config(page_title="Comparador de Empenhos", page_icon="")
+st.markdown("### 📑 COMPARADOR DE EMPENHOS")
 
-st.sidebar.title("Upload de Arquivos")
+st.sidebar.title("Upload dos Arquivos")
 
     # Upload do primeiro CSV
-uploaded_file_1 = st.sidebar.file_uploader("Empenhos do Relatório", type=["csv"])
+uploaded_file_1 = st.sidebar.file_uploader("Carregue abaixo o arquivo referente ao relatório de 'empenhos emitios' gerados pelos seu sistema", type=["csv"])
 if uploaded_file_1 is not None:
     df1 = pd.read_csv(uploaded_file_1)
     st.write("## Empenhos do Relatório")
     st.dataframe(df1)
 
     # Upload do segundo CSV
-uploaded_file_2 = st.sidebar.file_uploader("Empenhos Planilhados", type=["csv"])
+uploaded_file_2 = st.sidebar.file_uploader("Carregue abaixo o arquivo referente aos empenhos planilhados", type=["csv"])
 if uploaded_file_2 is not None:
     df2 = pd.read_csv(uploaded_file_2)
     st.write("## Empenhos Planilhados")
