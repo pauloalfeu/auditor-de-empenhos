37536c778d12ba5fde3be729b036e450cde003b6
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-12-06 09:00:01 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,20 @@
 import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
-
-import streamlit as st
 import pandas as pd
 
-def main():
-    st.set_page_config(page_title="Comparador de Empenhos", page_icon="")
+st.set_page_config(page_title="Comparador de Empenhos", page_icon="")
 
-    st.sidebar.title("Upload de Arquivos")
+st.sidebar.title("Upload de Arquivos")
 
     # Upload do primeiro CSV
-    uploaded_file_1 = st.sidebar.file_uploader("Empenhos do Relatório", type=["csv"])
-    if uploaded_file_1 is not None:
-        df1 = pd.read_csv(uploaded_file_1)
-        st.write("## Empenhos do Relatório")
-        st.dataframe(df1)
+uploaded_file_1 = st.sidebar.file_uploader("Empenhos do Relatório", type=["csv"])
+if uploaded_file_1 is not None:
+    df1 = pd.read_csv(uploaded_file_1)
+    st.write("## Empenhos do Relatório")
+    st.dataframe(df1)
 
     # Upload do segundo CSV
-    uploaded_file_2 = st.sidebar.file_uploader("Empenhos Planilhados", type=["csv"])
-    if uploaded_file_2 is not None:
-        df2 = pd.read_csv(uploaded_file_2)
-        st.write("## Empenhos Planilhados")
-        st.dataframe(df2)
-
-if __name__ == "__streamlit_app__":
-    main()
+uploaded_file_2 = st.sidebar.file_uploader("Empenhos Planilhados", type=["csv"])
+if uploaded_file_2 is not None:
+    df2 = pd.read_csv(uploaded_file_2)
+    st.write("## Empenhos Planilhados")
+    st.dataframe(df2)
