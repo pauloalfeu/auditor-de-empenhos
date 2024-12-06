537639f71b90ad96597fc49b147413eac1b8b662
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-12-06 10:48:46 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,9 +30,9 @@ if (uploaded_file_1 is not None) and (uploaded_file_2 is not None):
     # Função para comparar os DataFrames e retornar um novo DataFrame com as diferenças
     def comparar_empenhos(df1, df2):
         # Renomear a coluna 'Empenho' do df1 para garantir a comparação
-        df1 = df1.rename(columns={'Empenho': 'EMPENHO'})
+        #df1 = df1.rename(columns={'Empenho': 'EMPENHO'})
         # Renomeando a coluna 'A' para 'Coluna_A'
-        #df1['Empenho'] = df1['Empenho'].rename('EMPENHO')
+        df1['Empenho'] = df1['Empenho'].rename('EMPENHO')
 
         # Encontrar os empenhos que estão apenas em um dos DataFrames
         empenhos_unicos_df1 = df1[~df1['EMPENHO'].isin(df2['EMPENHO'])]
