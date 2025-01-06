4400aa1a0031394e5c90d4005747986224f13362
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2025-01-06 12:51:23 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ if (uploaded_file_1 is not None) and (uploaded_file_2 is not None):
 
 
     # Exibir o DataFrame com as diferenças
-    diferencas.drop(columns=['Espécie', 'Dotação', 'Vínculo', 'Estornado', 'Revertido', 'Líquido', 'EMPENHO', 'ANO', 'PLANILHA' ], inplace=True)
+    #diferencas.drop(columns=['Espécie', 'Dotação', 'Vínculo', 'Estornado', 'Revertido', 'Líquido', 'EMPENHO', 'ANO', 'PLANILHA' ], inplace=True)
     # Removendo a coluna sem nome (índice 1)
     diferencas = diferencas.dropna(axis=1, how='all')
     st.data_editor(diferencas)
